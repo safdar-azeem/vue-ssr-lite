ea176e54faf63d3c4c87dcbf5296dab7945819ad
safdar-azeem <safdarazeem00@gmail.com>
2026-07-19 07:38:43 +0500

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -2,6 +2,7 @@ export * from './SsrBrowserRuntime'
 export * from './SsrReactivityRuntime'
 export {
   SSR_REQUEST_RESOLUTION,
+  useSsrResolution,
   type SsrRequestResolution,
 } from './SsrRequestResolution'
 export type * from './SsrRuntimeTypes'
