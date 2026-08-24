ac5ed78feb9b4874f70a30ee49f3f1fcf5869a33
safdar-azeem <safdarazeem00@gmail.com>
2026-08-24 06:44:43 +0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,14 @@ export * from './server/SsrHtmlRuntime'
 export * from './server/SsrResponseCacheRuntime'
 export * from './server/SsrRuntimeConfigRuntime'
 export * from './server/SsrServerRuntime'
+export {
+  defineSitemap,
+  resolveSitemapConfigPath,
+} from './server/SsrSitemapConfig'
+export type { SitemapContext, SitemapEntry, SitemapProvider } from './extensions/seo/sitemap'
 export * from './SsrConfigCompileRuntime'
 export * from './SsrDomainRuntime'
 export * from './SsrRenderRuntime'
+export { useSsrRequestContext } from './SsrRequestContext'
 export type * from './SsrRuntimeTypes'
 export type * from './SsrConfigTypes'
