fd46a9111b884fbc858891a3184558eaf9c9093e
safdar-azeem <safdarazeem00@gmail.com>
2026-08-25 07:49:42 +0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,77 @@
-export * from './server/SsrAssetRuntime'
-export * from './server/SsrHostRuntime'
-export * from './server/SsrHtmlRuntime'
-export * from './server/SsrResponseCacheRuntime'
-export * from './server/SsrRuntimeConfigRuntime'
-export * from './server/SsrServerRuntime'
+/** Server/configuration APIs. Implementation runtimes are intentionally private. */
+export { defineSsrConfig } from './SsrConfigRuntime'
+export { defineSitemap } from './server/SsrSitemapConfig'
+export type {
+  SitemapContext,
+  SitemapEntry,
+  SitemapProvider,
+} from './extensions/seo/sitemap'
+
+/** Advanced programmatic hosting and cache integrations. */
 export {
-  defineSitemap,
-  resolveSitemapConfigPath,
-} from './server/SsrSitemapConfig'
-export type { SitemapContext, SitemapEntry, SitemapProvider } from './extensions/seo/sitemap'
-export * from './SsrConfigCompileRuntime'
-export * from './SsrDomainRuntime'
-export * from './SsrRenderRuntime'
+  createSsrManagedServer,
+  type SsrManagedServer,
+  type SsrManagedServerOptions,
+} from './server/SsrServerRuntime'
+export {
+  createSsrMemoryResponseCache,
+  type SsrMemoryResponseCacheOptions,
+} from './server/SsrResponseCacheRuntime'
+
+/** Reusable server-configuration helpers. */
+export {
+  createSsrConsoleLogger,
+  createSsrSeoEndpoints,
+  requireSsrEnum,
+  requireSsrEnv,
+  requireSsrHostname,
+  ssrEnvBoolean,
+  ssrEnvList,
+  ssrEnvNumber,
+  type SsrSeoEndpointMode,
+  type SsrSeoEndpointOptions,
+} from './server/SsrRuntimeConfigRuntime'
+
+/** Universal advanced helpers are mirrored here for server-only modules. */
+export { createDomainUrl, useSsrDomain } from './SsrDomainRuntime'
+export type {
+  SsrCreateDomainUrlOptions,
+  SsrDomainApi,
+} from './SsrDomainRuntime'
 export { useSsrRequestContext } from './SsrRequestContext'
-export type * from './SsrRuntimeTypes'
-export type * from './SsrConfigTypes'
+
+export type {
+  SsrApplicationConfig,
+  SsrApplicationCookiesConfig,
+  SsrApplicationDomainConfig,
+  SsrApplicationModuleRef,
+  SsrConfig,
+  SsrConfigExport,
+  SsrConfigServerOptions,
+  SsrConfigShared,
+  SsrDomainContext,
+  SsrDomainMode,
+  SsrDomainParamDefinition,
+  SsrDomainParamSource,
+  SsrMultiApplicationConfig,
+  SsrPublicConfigSource,
+  SsrRenderMode,
+  SsrSingleApplicationConfig,
+} from './SsrConfigTypes'
+export type {
+  SsrEndpointDefinition,
+  SsrEndpointTools,
+  SsrErrorRenderContext,
+  SsrHeaders,
+  SsrHeaderValue,
+  SsrHttpRequest,
+  SsrHttpResponse,
+  SsrLogger,
+  SsrReadinessProbe,
+  SsrRenderMetrics,
+  SsrResponseCache,
+  SsrResponseCacheInvalidation,
+  SsrResponseCacheReadOptions,
+  SsrResponseCacheStrategy,
+  SsrResponseCacheWriteOptions,
+} from './SsrRuntimeTypes'
