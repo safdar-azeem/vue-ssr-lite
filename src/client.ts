ff4ac0fb6f0e5053b53f4248adfcb7b6caab4895
safdar-azeem <safdarazeem00@gmail.com>
2026-08-25 07:49:42 +0500

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,35 @@
-export * from './SsrBrowserRuntime'
-export * from './SsrDomainRuntime'
-export * from './SsrReactivityRuntime'
+/** Advanced browser bootstrap APIs used by generated and custom integrations. */
+export {
+  hydrateSsrApplication,
+  mountSpaApplication,
+  type SsrClientApplicationDefinition,
+  type SsrHydrateOptions,
+  type SsrMountedApplication,
+  type SsrSpaMountOptions,
+} from './SsrBrowserRuntime'
+export {
+  createDomainUrl,
+  useSsrDomain,
+  type SsrCreateDomainUrlOptions,
+  type SsrDomainApi,
+} from './SsrDomainRuntime'
+export {
+  ssrWatch,
+  ssrWatchEffect,
+  type SsrWatchOptions,
+} from './SsrReactivityRuntime'
 export {
   SSR_REQUEST_RESOLUTION,
   useSsrResolution,
   type SsrRequestResolution,
 } from './SsrRequestResolution'
-export type * from './SsrRuntimeTypes'
+export {
+  SSR_HYDRATION_CONTEXT,
+  type SsrHydrationContext,
+} from './SsrHydrationRuntime'
+export type {
+  SsrApplicationDefinition,
+  SsrHydrationState,
+  SsrRenderRequest,
+} from './SsrRuntimeTypes'
+export type { SsrDomainContext } from './SsrConfigTypes'
