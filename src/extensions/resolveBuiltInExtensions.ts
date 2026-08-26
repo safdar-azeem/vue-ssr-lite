2a5b815b0d7247f16b73cc888599569d81467441
safdar-azeem <safdarazeem00@gmail.com>
2026-08-26 22:57:44 +0500

    Update resolveBuiltInExtensions.ts

--- src/extensions/resolveBuiltInExtensions.ts
+++ src/extensions/resolveBuiltInExtensions.ts
@@ -2,10 +2,12 @@ import type { ExtensionDefinition } from '../core/extensions/ExtensionDefinition
 import type { SsrApplicationDefinition } from '../SsrRuntimeTypes'
 import { createSeoExtension } from './seo/index'
 import { isSeoEnabled } from './seo/types'
+import type { SeoSiteDefaults } from './seo/types'
 
 export const resolveBuiltInExtensions = (
-  application: SsrApplicationDefinition<any, any>
+  application: SsrApplicationDefinition<any, any>,
+  siteDefaults?: SeoSiteDefaults
 ): ExtensionDefinition[] => {
   if (!isSeoEnabled(application.seo)) return []
-  return [createSeoExtension(application.seo)]
+  return [createSeoExtension(application.seo, siteDefaults)]
 }
