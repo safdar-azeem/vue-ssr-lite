c6a6913c7cfa56e2effe9306e21b91d035c42481
safdar-azeem <safdarazeem00@gmail.com>
2026-08-27 06:16:34 +0500

    Add App.vue

---
+++ examples/1-single-app/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="shell">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/posts/hello-world">Dynamic Post</RouterLink>
+    </nav>
+
+    <RouterView />
+  </div>
+</template>
