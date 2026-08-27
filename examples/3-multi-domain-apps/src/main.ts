a5c2ceb113999474efd1a8048bdfdb81bd580f65
safdar-azeem <safdarazeem00@gmail.com>
2026-08-28 00:42:26 +0500

    Update main.ts

--- examples/3-multi-domain-apps/src/main.ts
+++ examples/3-multi-domain-apps/src/main.ts
@@ -1,6 +1,7 @@
 import { createPinia } from 'pinia'
+import type { AppContext } from 'vue-ssr-lite'
 import './style.css'
 
-export default ({ app }: { app: any }) => {
+export default ({ app }: AppContext) => {
   app.use(createPinia())
 }
