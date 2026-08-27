506f7ad34614a2c3fceb2042ee596a11dc11338f
safdar-azeem <safdarazeem00@gmail.com>
2026-08-27 06:16:35 +0500

    Add LandingPage.vue

---
+++ examples/2-hybrid-route-app/src/modules/public/pages/LandingPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/pricing">Pricing</RouterLink>
+      <RouterLink to="/app">Open App</RouterLink>
+      <RouterLink to="/admin">Admin</RouterLink>
+    </nav>
+
+    <h1>Landing Page</h1>
+    <p>This route is server-rendered.</p>
+  </section>
+</template>
