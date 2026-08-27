5b6106d83b331a96c40742b3bebfe0c6f02bb887
safdar-azeem <safdarazeem00@gmail.com>
2026-08-28 00:42:28 +0500

    Add routes.ts

---
+++ fixtures/architecture-hybrid/src/routes.ts
@@ -0,0 +1,5 @@
+import publicRoutes from './modules/public/routes'
+import workspaceRoutes from './modules/workspace/routes'
+import adminRoutes from './modules/admin/routes'
+
+export default [...publicRoutes, ...workspaceRoutes, ...adminRoutes]
