750a16f65c6dd09c3280183f141f0b1c42bc7a1d
safdar-azeem <safdarazeem00@gmail.com>
2026-08-28 00:42:26 +0500

    Update server.ts

--- examples/2-hybrid-route-app/server.ts
+++ examples/2-hybrid-route-app/server.ts
@@ -4,25 +4,17 @@ import { sitemap } from './src/seo/sitemap'
 import { robots } from './src/seo/robots'
 
 export default defineServer({
-	// Default for the whole single application.
-	render: 'ssr',
+  // Default for the whole single application.
+  render: 'ssr',
 
-	server: {
-		port: 4211,
-		trustProxy: true,
-	},
+  server: {
+    port: 4212,
+    trustProxy: true,
+  },
 
-	seo: {
-		site: siteSeo,
-		sitemap,
-		robots,
-	},
-
-	// Proposed behavior:
-	// route.meta.render may override the default render mode.
-	//
-	// No applications array.
-	// No subdomains required.
-	//
-	// /src/main.ts and /src/App.vue are used automatically.
+  seo: {
+    site: siteSeo,
+    sitemap,
+    robots,
+  },
 })
