5ec3105c55ef8d3b431a2a424070b552f4b9f09b
safdar-azeem <safdarazeem00@gmail.com>
2026-08-27 06:16:35 +0500

    Add AdminLayout.vue

---
+++ examples/2-hybrid-route-app/src/modules/admin/AdminLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <nav>
+      <RouterLink to="/admin">Dashboard</RouterLink>
+      <RouterLink to="/admin/users">Users</RouterLink>
+      <RouterLink to="/admin/settings">Settings</RouterLink>
+      <a href="/">Public Site</a>
+    </nav>
+
+    <RouterView />
+  </section>
+</template>
