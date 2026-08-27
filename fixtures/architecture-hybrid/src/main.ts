7f5f11bc6dde2de8cfc0994f5b399eb7ac92b449
safdar-azeem <safdarazeem00@gmail.com>
2026-08-28 00:42:28 +0500

    Add main.ts

---
+++ fixtures/architecture-hybrid/src/main.ts
@@ -0,0 +1,8 @@
+import type { AppContext } from '../../../src/index'
+import routes from './routes'
+
+export { routes }
+
+export default (_context: AppContext) => {
+  // Hybrid plugins belong here.
+}
