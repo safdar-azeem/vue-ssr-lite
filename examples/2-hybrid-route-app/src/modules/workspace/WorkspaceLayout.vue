847138b89e1eccd5c7eb9810298ba00a1e851258
safdar-azeem <safdarazeem00@gmail.com>
2026-08-27 06:16:35 +0500

    Add WorkspaceLayout.vue

---
+++ examples/2-hybrid-route-app/src/modules/workspace/WorkspaceLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <nav>
+      <RouterLink to="/app">Workspace</RouterLink>
+      <RouterLink to="/app/projects">Projects</RouterLink>
+      <RouterLink to="/app/settings">Settings</RouterLink>
+      <a href="/">Public Site</a>
+    </nav>
+
+    <RouterView />
+  </section>
+</template>
