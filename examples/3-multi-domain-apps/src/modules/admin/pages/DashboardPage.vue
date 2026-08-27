4559e433a1a1cb5da1ad55e33bb6d35a314c6f7e
safdar-azeem <safdarazeem00@gmail.com>
2026-08-27 06:16:35 +0500

    Add DashboardPage.vue

---
+++ examples/3-multi-domain-apps/src/modules/admin/pages/DashboardPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+    <nav>
+      <RouterLink to="/">Dashboard</RouterLink>
+      <RouterLink to="/users">Users</RouterLink>
+    </nav>
+    <h1>Admin Dashboard</h1>
+    <p>SPA using the global main.ts and App.vue.</p>
+  </section>
+</template>
