dc873827ea361a1e01a3a481b111db5bc7712ee0
safdar-azeem <safdarazeem00@gmail.com>
2026-08-27 06:16:36 +0500

    Add HomePage.vue

---
+++ examples/3-multi-domain-apps/src/modules/website/pages/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/projects">Projects</RouterLink>
+    </nav>
+    <h1>Public Website</h1>
+    <p>SSR application using global main.ts and App.vue.</p>
+  </section>
+</template>
