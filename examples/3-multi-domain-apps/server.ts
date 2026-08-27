dc49756eefd6b3dd4db6217a6e545e3b6b3b5682
safdar-azeem <safdarazeem00@gmail.com>
2026-08-27 06:16:35 +0500

    Add server.ts

---
+++ examples/3-multi-domain-apps/server.ts
@@ -0,0 +1,19 @@
+import { defineServer } from 'vue-ssr-lite'
+
+import website from './src/modules/website/app'
+import admin from './src/modules/admin/app'
+import docs from './src/modules/docs/app'
+
+export default defineServer({
+	server: {
+		port: 4211,
+		trustProxy: true,
+	},
+
+	applications: [website, admin, docs],
+
+	// Global app config omitted intentionally.
+	// Defaults:
+	//   main -> /src/main.ts
+	//   root -> /src/App.vue
+})
