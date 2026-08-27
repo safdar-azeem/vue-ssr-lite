4589df9f6603d544e5d919b0b46a8da8cda8e708
safdar-azeem <safdarazeem00@gmail.com>
2026-08-27 06:16:36 +0500

    Add ProjectPage.vue

---
+++ examples/3-multi-domain-apps/src/modules/website/pages/ProjectPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { useSeo } from 'vue-ssr-lite'
+
+const route = useRoute()
+
+useSeo(computed(() => ({
+  title: `Project: ${String(route.params.slug)}`,
+  description: `Dynamic project SEO for ${String(route.params.slug)}.`,
+})))
+</script>
+
+<template>
+  <article>
+    <h1>Project: {{ route.params.slug }}</h1>
+  </article>
+</template>
