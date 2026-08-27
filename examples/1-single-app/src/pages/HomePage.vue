1d4466f957c1ca5909f3ce83beacb8157dbebdbc
safdar-azeem <safdarazeem00@gmail.com>
2026-08-27 06:16:34 +0500

    Add HomePage.vue

---
+++ examples/1-single-app/src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useCounterStore } from '../stores/counter'
+
+const counter = useCounterStore()
+</script>
+
+<template>
+  <section>
+    <h1>Home</h1>
+    <p>Pinia count: {{ counter.count }}</p>
+    <button @click="counter.increment()">Increment</button>
+  </section>
+</template>
