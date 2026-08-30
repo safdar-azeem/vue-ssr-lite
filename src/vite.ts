b0bbd04f1e9b6d4578e5fca981bb3ad591038451
safdar-azeem <safdarazeem00@gmail.com>
2026-08-30 08:42:08 +0500

    Update vite.ts

--- src/vite.ts
+++ src/vite.ts
@@ -1,2 +1,2 @@
 export { vueSsrLite } from './vite/SsrVitePlugin'
-export type { SsrViteApplicationEntry, SsrVitePluginOptions } from './vite/SsrVitePlugin'
+export type { SsrVitePluginOptions } from './vite/SsrVitePlugin'
