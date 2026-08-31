14e0d0d11c45a2435c97ca5e2ea2c5826c40b1cd
safdar-azeem <safdarazeem00@gmail.com>
2026-08-31 22:15:53 +0500

    Add AboutPage.vue

---
+++ fixtures/navigation-loading-consumer/src/AboutPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const note = ref('preserved')
+</script>
+
+<template>
+  <article class="about-page">
+    About page
+    <input v-model="note" class="about-note" />
+  </article>
+</template>
