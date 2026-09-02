4af3b6b854b94e1722d99eabec33c7d6e3948a21
safdar-azeem <safdarazeem00@gmail.com>
2026-09-03 03:06:04 +0500

    Update App.vue

--- examples/1-single-app/src/App.vue
+++ examples/1-single-app/src/App.vue
@@ -4,8 +4,17 @@
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
       <RouterLink to="/posts/hello-world">Dynamic Post</RouterLink>
+      <RouterLink to="/products">Products</RouterLink>
     </nav>
 
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <Suspense>
+        <component :is="Component" />
+
+        <template #fallback>
+          <p>Loading page…</p>
+        </template>
+      </Suspense>
+    </RouterView>
   </div>
 </template>
