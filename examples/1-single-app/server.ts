3196e48053e2617e90b41dd631e2a9d44847c8b9
safdar-azeem <safdarazeem00@gmail.com>
2026-09-05 03:18:17 +0500

    Update server.ts

--- examples/1-single-app/server.ts
+++ examples/1-single-app/server.ts
@@ -2,23 +2,26 @@ import { defineServer } from 'vue-ssr-lite'
 import { siteSeo } from './src/seo/site'
 import { sitemap } from './src/seo/sitemap'
 import { robots } from './src/seo/robots'
-
+import { loggerMiddleware } from './src/loggerMiddleware'
 export default defineServer({
-	render: 'ssr',
+  render: 'ssr',
 
-	server: {
-		port: 4211,
-		trustProxy: true,
-	},
+  server: {
+    port: 4211,
+    trustProxy: true,
+  },
 
-	seo: {
-		site: siteSeo,
-		sitemap,
-		robots,
-	},
+  seo: {
+    site: siteSeo,
+    sitemap,
+    robots,
+  },
 
-	// No `app` config required.
-	// Defaults:
-	//   main -> /src/main.ts
-	//   root -> /src/App.vue
+  // Global middleware runs for every route.
+  middleware: [loggerMiddleware],
+
+  // No `app` config required.
+  // Defaults:
+  //   main -> /src/main.ts
+  //   root -> /src/App.vue
 })
