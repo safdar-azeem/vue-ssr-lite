93adc39c430e66f7c54a234e1af257f1228e2cfe
safdar-azeem <safdarazeem00@gmail.com>
2026-09-05 05:57:54 +0500

    Delete loggerMiddleware.ts

--- examples/1-single-app/src/loggerMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import { defineMiddleware } from 'vue-ssr-lite'
-
-export const loggerMiddleware = defineMiddleware(({ to, from, server }) => {
-  console.log('[middleware]', {
-    runtime: server ? 'server' : 'browser',
-    from: from?.fullPath ?? null,
-    to: to.fullPath,
-  })
-})
