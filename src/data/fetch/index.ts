a9cc84056dc4aa94cca2ba4c8cb1e621b6ccebc6
safdar-azeem <safdarazeem00@gmail.com>
2026-09-06 19:34:31 +0500

    Add index.ts

---
+++ src/data/fetch/index.ts
@@ -0,0 +1,17 @@
+export { useFetch } from './composables/useFetch'
+export type {
+  UseFetchDoneContext,
+  UseFetchError,
+  UseFetchErrorContext,
+  UseFetchOptions,
+  UseFetchOptionsBase,
+  UseFetchOptionsParameter,
+  UseFetchPolicy,
+  UseFetchResult,
+  UseFetchReturn,
+  UseFetchVariablePrimitive,
+  UseFetchVariableShape,
+  UseFetchVariableValue,
+  UseFetchVariables,
+  VariablesOption,
+} from './types/SsrFetchTypes'
