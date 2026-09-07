77143f1da30a44e6fe587cccbc6c81ef2a0ab886
safdar-azeem <safdarazeem00@gmail.com>
2026-09-07 07:38:28 +0500

    Delete index.ts

--- doc/server-api-plan/server-api-example/server/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { healthRoutes } from './health'
-import { organizationRoutes } from './organizations'
-import { productsRoutes } from './products'
-
-export const serverRoutes = [
-  healthRoutes,
-  productsRoutes,
-  organizationRoutes,
-]
