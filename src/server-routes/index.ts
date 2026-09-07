4e18630c3ac5c92044851f7976e542def9605ebc
safdar-azeem <safdarazeem00@gmail.com>
2026-09-07 06:30:53 +0500

    Add index.ts

---
+++ src/server-routes/index.ts
@@ -0,0 +1,12 @@
+export { defineServerMiddleware } from './defineServerMiddleware'
+export { defineServerRoutes } from './defineServerRoutes'
+export type {
+  GlobalServerMiddleware,
+  ServerMiddleware,
+  ServerMiddlewareHandler,
+  ServerMiddlewareBaseContext,
+  ServerRouteContext,
+  ServerRouteHandler,
+  ServerRouteMethod,
+  ServerRoutesDefinition,
+} from './SsrServerRouteTypes'
